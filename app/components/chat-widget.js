eefb6747f7e87555b7cefd4918dd2c038a508690
Andrean Nechev <andrean.nechev@gmail.com>
2017-07-19 13:02:11 +0300

    userInput rendered on the page from component.js

--- app/components/chat-widget.js
+++ app/components/chat-widget.js
@@ -4,6 +4,11 @@ export default Ember.Component.extend({
   ajax: Ember.inject.service('ajax'),
   actions: {
     setThePayload(){
+      // Fetch User input
+      let userQuery = this.get('userQuery');
+      console.log(userQuery);
+
+      //AXAJ call to the Service
       let ajax = Ember.get(this, 'ajax');
       // ajax.setPayload('here is the new payload');
       // console.log(ajax.myPayload);
