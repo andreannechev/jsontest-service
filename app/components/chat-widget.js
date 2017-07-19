97f0c7b1e17c4ab835c628bae7dbd0ed8c778b63
Andrean Nechev <andrean.nechev@gmail.com>
2017-07-19 10:55:12 +0300

    dummy test
    save string from template.js action to property in the service and
    console.log it from the same template.js action

--- app/components/chat-widget.js
+++ app/components/chat-widget.js
@@ -3,22 +3,28 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   ajax: Ember.inject.service('ajax'),
   actions: {
-    sendRequest() {
-      return this.get('ajax').request('query', {
-        host: 'https://api.api.ai',
-        namespace: 'v1',
-        contentType: 'application/json; charset=utf-8',
-        headers : {"Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'},
-        method: 'POST',
-        data: {query: "lets go", lang: "en", sessionId: "somerandomthing"},
-        success: function(data) {
-          // var payload = JSON.stringify(data, undefined, 2);
-          let spokenResponse = data.result.speech
-          console.log(spokenResponse);
-
-        }
-
-      });
-    }
+    setThePayload(){
+      console.log('we are setting new payload');
+      let ajax = Ember.get(this, 'ajax');
+      ajax.setPayload('here is the new payload');
+      console.log(ajax.myPayload);
+    },
+    // sendRequest() {
+    //   return this.get('ajax').request('query', {
+    //     host: 'https://api.api.ai',
+    //     namespace: 'v1',
+    //     contentType: 'application/json; charset=utf-8',
+    //     headers : {"Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'},
+    //     method: 'POST',
+    //     data: {query: "lets go", lang: "en", sessionId: "somerandomthing"},
+    //     success: function(data) {
+    //       // var payload = JSON.stringify(data, undefined, 2);
+    //       let spokenResponse = data.result.speech
+    //       console.log(spokenResponse);
+    //
+    //     }
+    //
+    //   });
+    // }
   }
 });
--- app/services/ajax.js
+++ app/services/ajax.js
@@ -2,12 +2,15 @@ import Ember from 'ember';
 import AjaxService from 'ember-ajax/services/ajax';
 
 export default AjaxService.extend({
-  // host: 'https://api.api.ai',
-  // namespace: 'v1',
-  // contentType: 'application/json; charset=utf-8',
-  // headers : {
-  // "Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'
-  // },
+  myPayload: null,
+
+  setPayload(payload) {
+    this.set('myPayload', payload);
+  },
+
+  // getPayload(){
+  //   return this.get('myPayload');
+  // }
 
 
 });
