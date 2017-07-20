ec7f512812aedce7333dea7b1d7056c324c1624a
Andrean Nechev <andrean.nechev@gmail.com>
2017-07-20 11:11:15 +0300

    implement .then in the component

--- app/components/chat-widget.js
+++ app/components/chat-widget.js
@@ -8,31 +8,24 @@ export default Ember.Component.extend({
       let userQuery = this.get('userQuery');
       console.log(userQuery);
 
+
       //AXAJ call to the Service
       let ajax = Ember.get(this, 'ajax');
       // ajax.setPayload('here is the new payload');
       // console.log(ajax.myPayload);
 
-      let test = ajax.sendRequest(userQuery);
-      ajax.setPayload(test);
-      console.log(ajax.myPayload);
+      let test = ajax.sendRequest(userQuery)
+      .then(function(data){
+        ajax.setPayload(data.result.speech);
+        console.log(ajax.myPayload);
+      });
+
+      // Cleat user input
+      this.setProperties({
+        userQuery: ''
+      });
+
      },
-    // sendRequest() {
-    //   return this.get('ajax').request('query', {
-    //     host: 'https://api.api.ai',
-    //     namespace: 'v1',
-    //     contentType: 'application/json; charset=utf-8',
-    //     headers : {"Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'},
-    //     method: 'POST',
-    //     data: {query: "lets go", lang: "en", sessionId: "somerandomthing"},
-    //     success: function(data) {
-    //       // var payload = JSON.stringify(data, undefined, 2);
-    //       let spokenResponse = data.result.speech
-    //       console.log(spokenResponse);
-    //
-    //     }
-    //
-    //   });
-    // }
+
   }
 });
--- app/services/ajax.js
+++ app/services/ajax.js
@@ -2,29 +2,67 @@ import Ember from 'ember';
 import AjaxService from 'ember-ajax/services/ajax';
 
 export default AjaxService.extend({
-  myPayload: 'null',
+  myPayload: null,
 
   setPayload(payload) {
     this.set('myPayload', payload);
   },
 
-  // Method to call ajax
-  sendRequest(userQuery){
-      return this.request('query', {
-      host: 'https://api.api.ai',
-      namespace: 'v1',
-      contentType: 'application/json; charset=utf-8',
-      headers : {"Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'},
-      method: 'POST',
-      data: {query: userQuery, lang: "en", sessionId: "somerandomthing"},
-      success: function(data) {
-        // console.log(JSON.stringify(data))
-        var name = data.id;
-        console.log(name);
-      }
+  // Handle promise
 
-    });
-  }
+
+  sendRequest(userQuery){
+      // Will be call from chat widget
+      return this.request('query', {
+        host: 'https://api.api.ai',
+        namespace: 'v1',
+        contentType: 'application/json; charset=utf-8',
+        headers : {"Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'},
+        method: 'POST',
+        data: {query: userQuery, lang: "en", sessionId: "somerandomthing"},
+        success: function(data) {
+          }
+      })
+
+      // after become results
+      // .then(function (data) {
+      //
+      //   });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Method to call ajax
+
+  // sendRequest(userQuery){
+  //     return this.request('query', {
+  //       host: 'https://api.api.ai',
+  //       namespace: 'v1',
+  //       contentType: 'application/json; charset=utf-8',
+  //       headers : {"Authorization": 'Bearer 0f46317926264d0eb3d0eaf804b6831d'},
+  //       method: 'POST',
+  //       data: {query: userQuery, lang: "en", sessionId: "somerandomthing"},
+  //       success: function(data) {
+  //         // console.log(JSON.stringify(data))
+  //         // var name = data.id;
+  //         // console.log(name);
+  //         let myPayload = Ember.get(this, 'myPayload');
+  //
+  //         // Ember.get(this, 'setPayload(data.id)');
+  //         console.log(myPayload);
+  //       }
+  //     });
+  //   }
 
 
   // getPayload(){
